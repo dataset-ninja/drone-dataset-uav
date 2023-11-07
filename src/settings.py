0eebbbde3546bb966bedc313716a8d7de63e2b41
Arsenii Vladimirov <78355358+grokhi@users.noreply.github.com>
2023-11-07 19:24:58 +0600

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -57,6 +57,7 @@ REPOSITORY: Optional[Union[str, List[str], Dict[str, str]]] = {"GitHub":"https:/
 
 CITATION_URL: Optional[str] = None
 AUTHORS: Optional[List[str]] = ["Mehdi Özel"]
+AUTHORS_CONTACTS: Optional[List[str]] = ["mehdixozel@gmail.com", "https://github.com/dasmehdix","https://www.linkedin.com/in/mehdixozel"]
 
 ORGANIZATION_NAME: Optional[Union[str, List[str]]] = None
 ORGANIZATION_URL: Optional[Union[str, List[str]]] = None
@@ -110,6 +111,7 @@ def get_settings():
     settings["repository"] = REPOSITORY
     settings["citation_url"] = CITATION_URL
     settings["authors"] = AUTHORS
+    settings["authors_contacts"] = AUTHORS_CONTACTS
     settings["organization_name"] = ORGANIZATION_NAME
     settings["organization_url"] = ORGANIZATION_URL
     settings["slytagsplit"] = SLYTAGSPLIT
